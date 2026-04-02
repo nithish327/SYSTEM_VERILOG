69d3d0a4f6511449dda7c2d6bb1f40c2277359ad
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-04-02 12:47:21 +0530

    Create logic_implication.sv for logical implication tests
    Implement logic implication module with test cases for various input combinations.

---
+++ OPERATORS/LOGICAL_IMPLICATION/logic_implication.sv
@@ -0,0 +1,37 @@
+module arithmtic_operator;
+  logic [4:0] a,b;
+  int x;
+  initial begin;
+    a= 0;
+    b= 'b00;
+    if(a->b)
+      $display("True (%0d)",(a->b));
+    else
+      $display("False (%0d)",(a->b));
+    
+    a= 'b00;
+    b= 'b01;
+    if(a->b)
+      $display("True (%0d)",(a->b));
+    else
+      $display("False (%0d)",(a->b));
+    
+    a= 1;
+    b= 0;
+    if(a->b)
+      $display("True (%0d)",(a->b));
+    else
+      $display("False (%0d)",(a->b));
+    
+    a= 1;
+    b= 1;
+    if(a->b)
+      $display("True (%0d)",(a->b));
+    else
+      $display("False (%0d)",(a->b));
+    
+    
+ 
+  end
+endmodule
+    
