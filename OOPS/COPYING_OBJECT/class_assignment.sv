2bdc3a51337a84e2a30359207a0fdab0302db624
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-04-08 12:51:03 +0530

    Modify display function to include name parameter
    Updated display function to accept a name parameter and modified calls accordingly.

--- OOPS/COPYING_OBJECT/class_assignment.sv
+++ OOPS/COPYING_OBJECT/class_assignment.sv
@@ -3,8 +3,8 @@ class packet_1;
   function new(int a);
     data = a;
   endfunction
-  function display();
-    $display("packet data = %0d ",data);
+  function display(string name);
+    $display("%0s = %0d ",name,data);
   endfunction
   
 endclass
@@ -13,9 +13,11 @@ module tb;
   packet_1 pkt_1,pkt_2;
   initial begin
     pkt_1 = new(23);
-    pkt_1.display;
+    pkt_1.display("pkt_1");
     pkt_2 = pkt_1;
-    pkt_2.display;
+    pkt_2.display("pkt_2");
+    pkt_2.data = 26;
+    pkt_1.display("pkt_1");
   end
 endmodule
     
