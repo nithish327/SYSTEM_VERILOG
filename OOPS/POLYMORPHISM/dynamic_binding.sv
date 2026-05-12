ba075bce98e17fdadedb4ceb5993ed0eb6cf5829
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-12 16:56:07 +0530

    Add dynamic binding example with employee and manager classes
    Implemented dynamic binding in SystemVerilog with employee and manager classes. The manager class overrides the display method to show its specific salary details.

---
+++ OOPS/POLYMORPHISM/dynamic_binding.sv
@@ -0,0 +1,39 @@
+class employee;
+  int monthly_salary =22000;
+  int lpa;
+  function new();
+    lpa = monthly_salary * 12;
+  endfunction
+  virtual function void display();
+    $display("Employee salary");
+    $display("monthly_salary = %0d LPA = %0d",monthly_salary,lpa);
+  endfunction
+endclass
+
+class manager extends employee;
+  int monthly_salary ;
+  int lpa = 2400000;
+  function new();
+    monthly_salary = lpa / 12;
+  endfunction
+  function void display();
+    $display("Manager salary");
+    $display("monthly_salary = %0d LPA = %0d",monthly_salary,lpa);
+  endfunction
+endclass
+
+module salary;
+  employee e;
+  manager m;
+  initial begin
+    m = new();
+    e=m;// Parent handle points to child object
+    e.display();// dynamic handling
+  end
+endmodule
+
+/* OUTPUT 
+Manager salary
+# monthly_salary = 200000 LPA = 2400000
+# exit
+*/
