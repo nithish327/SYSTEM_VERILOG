7dce65471be4db641bf3683da07847ed0d492b68
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-14 18:19:37 +0530

    Create randc_keyword_ex.sv

---
+++ RANDOMIZATION/RAND&RANDC_KEYWORD/randc_keyword_ex.sv
@@ -0,0 +1,18 @@
+class packet;
+  randc bit [2:0]data;
+  function void display();
+    $display("data = %0d",data);
+  endfunction
+endclass
+
+module tb;
+  packet p;
+  initial  begin
+    p =new();
+    repeat(8)begin
+      p.randomize();
+      p.display();
+    end
+  end
+endmodule
+  
